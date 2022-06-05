09dbaf3e595a0b4a7eb5e3e227e669c8bd31bb9c
yunus <yunusolcar@hotmail.com>
2022-06-05 22:23:15 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Php</title>
+</head>
+
+<body>
+
+    Bu yazı HTML'dir. <br><br>
+
+    <?php
+    echo "<b>Yunus Olcar</b>
+      <br><br>
+    <u>Web Programlama Bootcamp'i</u>
+    <br><br>
+      <s>Php ile Web Programlama</s>
+      ";
+    ?>
+
+</body>
+
+</html>
